c7f5bbc6f565cfde4708c785af6f5f77d3a83447
Jon Powell <jpowell68601@gmail.com>
2021-10-14 16:58:50 -0500

    Setup Header component and styles

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import Homepage from './pages/Homepage/Homepage';
 import ShopPage from './pages/Shop/ShopPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
